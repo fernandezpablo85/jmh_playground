5a526a334a47a1a13ede149cbdf3420c22e05a80
Pablo Fernandez <fernandezpablo85@gmail.com>
2018-08-03 10:08:18 -0300

    danger of optimizations

---
+++ src/main/java/org/despegar/jcip/Optimizations.java
@@ -0,0 +1,44 @@
+package org.despegar.jcip;
+
+import org.openjdk.jmh.annotations.*;
+
+import java.text.DateFormat;
+import java.text.Format;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+public class Optimizations
+{
+  @State(Scope.Benchmark)
+  public static class DoubleHolder
+  {
+    double val = 10;
+  }
+
+  @Benchmark
+  public void doNothing()
+  {
+    // delivers.
+  }
+
+  @Benchmark
+  public void logImpl(DoubleHolder dh)
+  {
+    Math.log(dh.val);
+  }
+
+  @Benchmark
+  public double logImplTwo()
+  {
+    return Math.log(10);
+  }
+
+  @Benchmark
+  public double logImplThree(DoubleHolder dh)
+  {
+    return Math.log(dh.val);
+  }
+
+}
